eb7fba3b72bc58122b0e5df957644f988e17d9f5
AbhiCodes <chhettriabhishek9@gmail.com>
2024-07-13 11:15:13 -0400

    api usage

--- src/StockNews.jsx
+++ src/StockNews.jsx
@@ -3,28 +3,28 @@ import React, { useEffect, useState } from "react";
 function StockNews() {
   const [market, setMarket] = useState();
   const [searchTerm, setSearchTerm] = useState("");
-  useEffect(() => {
-    const getData = async () => {
-      const url = `https://devapi-finance.p.rapidapi.com/v2/markets/stock/ticker-summary?ticker=${searchTerm}&type=STOCKS`;
-      const options = {
-        method: "GET",
-        headers: {
-          "x-rapidapi-key":
-            "aaf412f001mshb16825a0d5065e6p181d8ajsn4a009ea4951a",
-          "x-rapidapi-host": "devapi-finance.p.rapidapi.com",
-        },
-      };
-
-      try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        setMarket(result.body.summaryData.MarketCap.value);
-      } catch (error) {
-        console.error(error);
-      }
+  const getData = async () => {
+    const url = `https://devapi-finance.p.rapidapi.com/v2/markets/stock/ticker-summary?ticker=${searchTerm}&type=STOCKS`;
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "aaf412f001mshb16825a0d5065e6p181d8ajsn4a009ea4951a",
+        "x-rapidapi-host": "devapi-finance.p.rapidapi.com",
+      },
     };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setMarket(result.body.summaryData.MarketCap.value);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
     getData();
-  }, []);
+  }, [searchTerm]);
   return (
     <div>
       <div className="p-5 text-purple-800 text-center">
